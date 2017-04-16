3d8eb2bda96cd9a40819946ea53bc9aefcac6aa4
AJ Weeks <ajweek@shaw.ca>
2017-04-16 13:49:10 +0200

    Fix release settings

--- source/main.cpp
+++ source/main.cpp
@@ -15,14 +15,18 @@
 
 int main(int argc, const char* argv[])
 {
+#if defined(DEBUG) || defined(_DEBUG)
 	//_CrtSetBreakAlloc(18);
+#endif
 
 	TextAdventure* game = new TextAdventure();
 
 	const std::string worldFilePath = argv[1];
 	game->Run(worldFilePath);
 
+#if defined(DEBUG) || defined(_DEBUG)
 	_CrtDumpMemoryLeaks();
-	
+#endif 
+
 	delete game;
 }
